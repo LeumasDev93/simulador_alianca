302145f9f27964728a8328e2cb990d46dfba3c92
Cursos Drive <aliancaseguros2025@gmail.com>
2025-11-20 15:40:59 -0100

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import Footer from "@/components/Footer";
-import SimulationScreen from "@/components/simulation/SimulationScreen";
+import { useProducts } from "@/hooks/useProducts";
+import { LoadingContainer } from "@/components/ui/loading-container";
+import { FaExclamationTriangle } from "react-icons/fa";
+import ProductsTab from "@/components/simulation/ProductsTab";
 
 export default function Page() {
+	const { products, loading, error } = useProducts();
 	return (
 		<main className="flex flex-col px-4 md:px-10 pt-8 pb-4 gap-8 min-h-screen bg-gradient-to-b from-gray-200 to-white">
 		
@@ -15,7 +19,38 @@ export default function Page() {
 					Simule, compare e escolha o seguro que melhor se adapta a si
 				</h2>
 			</div>
-			<SimulationScreen />
+			<section className="w-full flex flex-col items-center justify-center flex-1 py-8">
+
+				{loading && (
+				<LoadingContainer message="CARREGANDO SIMULAÇÕES..." />
+				)}
+				{error && (
+				<div className="flex items-center gap-3 bg-gradient-to-r from-red-50 to-red-100 border-2 border-red-300 text-red-800 px-6 py-4 rounded-2xl mb-6 shadow-lg">
+					<div className="bg-red-200 p-2 rounded-full">
+					<FaExclamationTriangle className="h-6 w-6 text-red-600" />
+					</div>
+					<div>
+					<p className="font-bold text-base">Ops! Algo deu errado</p>
+					<p className="text-sm">
+						Erro ao carregar os produtos. Tente novamente mais tarde.
+					</p>
+					</div>
+				</div>
+				)}
+
+				{!loading && !error && products && products.length > 0 && (
+				<ProductsTab
+					loading={loading}
+					error={error}
+					products={products}
+					onSelect={(p) => {
+					window.location.href = `/simulaction?productId=${p.productId}`;
+					}}
+				/>
+				)}
+
+
+				</section>
 			<Footer />
 		</main>
 	);
